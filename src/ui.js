a026c56ee4c812491bcad6772d54ec7a0066f410
James McNeill <mcneja@gmail.com>
2026-03-05 12:00:40 -0800

    Whoops; remove the hotkey help for toggling SDF renderer]\

--- src/ui.js
+++ src/ui.js
@@ -19,5 +19,5 @@ export function updateHud(hud, stats){
   }
   const debugSuffix = stats.debug ? ` | miner candidates: ${stats.minerCandidates}` : "";
   hud.textContent =
-    `fps: ${stats.fps} | hull: ${stats.shipHp} | level: ${stats.level} | state: ${stats.state} | speed: ${stats.speed.toFixed(2)} | miners: ${stats.miners} | dead: ${stats.minersDead} | verts: ${stats.verts.toLocaleString()} | air: ${stats.air.toFixed(3)}${debugSuffix} | LMB: shoot | RMB: bomb | M: new map | N: next level | V: toggle render | C: debug collisions | R: restart`;
+    `fps: ${stats.fps} | hull: ${stats.shipHp} | level: ${stats.level} | state: ${stats.state} | speed: ${stats.speed.toFixed(2)} | miners: ${stats.miners} | dead: ${stats.minersDead} | verts: ${stats.verts.toLocaleString()} | air: ${stats.air.toFixed(3)}${debugSuffix} | LMB: shoot | RMB: bomb | M: new map | N: next level | C: debug collisions | R: restart`;
 }
